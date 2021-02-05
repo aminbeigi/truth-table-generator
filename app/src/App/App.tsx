cce55ab61bba38a97a131c69242ebe974a9e2133
Amin Ghasembeigi <me@aminbeigi.com>
2021-02-05 20:16:38 +1100

    Only accept chars in list

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -8,12 +8,14 @@ export const App: React.FC = () => {
     const [value, setValue] = useState('');
 
     const onChangeHandler = (e: any) => {
-        let html_value = e.target.value
-
-        html_value = html_value.replace('a', 'z');
-        setValue(html_value);
-        console.log(value);
+        let html_value: string = e.target.value
+        // TODO: don't accept characters not in set 
+        html_value = html_value.replace(/[^a-zA-Z0-9/-]/, '');
+        setValue(html_value)
+        e.target.value = html_value 
+        console.log(value)
     }
+
     return (
         <div>
             <ExpressionField onChangeHandler={onChangeHandler}/>
