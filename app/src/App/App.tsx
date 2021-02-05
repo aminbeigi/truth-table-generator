31fda33754b6510f22b855fa58befc75ec701250
Amin Ghasembeigi <me@aminbeigi.com>
2021-02-05 22:57:43 +1100

    Remove numbers from allowd inputs

--- app/src/App/App.tsx
+++ app/src/App/App.tsx
@@ -11,7 +11,7 @@ export const App: React.FC = () => {
     // TODO: should be react functional comp
     const OnChangeHandler = (e: any) => {
         let html_value: string = e.target.value
-        html_value = html_value.replace(/[^a-zA-Z0-9|&∨∧]/, '');
+        html_value = html_value.replace(/[^a-zA-Z|&∨∧]/, '');
         html_value = html_value.replace('||', '∨');
         html_value = html_value.replace('&&', '∧');
         setValue(html_value)
