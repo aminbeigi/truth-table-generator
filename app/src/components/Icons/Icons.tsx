2dab7b2144a561fd60db44289574c7e2ebcb71f1
Amin Ghasembeigi <me@aminbeigi.com>
2021-02-06 15:26:01 +1100

    Add buttons

--- app/src/components/Icons/Icons.tsx
+++ app/src/components/Icons/Icons.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Icons: React.FC = () => {
     return (
-        <div>hai</div>
+        <div>
+            <div><i className="fab fa-github"></i></div>
+            <div><i className="far fa-question-circle"></i></div>
+        </div> 
     )
 }
