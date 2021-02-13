1026a95fc77f38e45c3f3965db661790cb0a8d83
Amin Ghasembeigi <me@aminbeigi.com>
2021-02-14 00:50:34 +1100

    Download fonts

--- app/src/App/styled.js
+++ app/src/App/styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Title = styled.h1`
     text-align: center;
     font-size: 4.5rem;
-    font-family: 'Major Mono Display',Courier New;
+    font-family: 'Major Mono Display';
     margin-top: 25px;
 `;
 
