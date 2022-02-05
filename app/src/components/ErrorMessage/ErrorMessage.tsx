8237c870a2863462097838cef8191011f475a84a
aminbeigi <me@aminbeigi.com>
2022-02-05 15:47:40 +1100

    Remove console log

--- app/src/components/ErrorMessage/ErrorMessage.tsx
+++ app/src/components/ErrorMessage/ErrorMessage.tsx
@@ -21,9 +21,6 @@ export const ErrorMessage: React.FC<Props> = ({errorObject}) => {
     let index = errorObject['index'];
     let operator = value[index];
     const array = [value.slice(0,index), value.slice(index+1)]; // only slice first occurence
-    console.log('value: ', value)
-    console.log('index: ', index)
-    console.log(array)
     return (
         <Wrapper>
             <SyntaxOkay>{array[0]}<SyntaxBad>{operator}</SyntaxBad>{array[1]}</SyntaxOkay>
